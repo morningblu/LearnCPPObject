98760aa052d5556af455a210509a6b636dd8fd48
kdvictor <huanliu_uestc@163.com>
2019-09-03 06:52:14 +0800

    开发者角度优化之后

--- LearnCppObject/2.8_程序优化.cpp
+++ LearnCppObject/2.8_程序优化.cpp
@@ -24,13 +24,23 @@ public:
 	}
 };
 
+//开发者视角
 CTempValue Double(CTempValue& ts)
 {
-	CTempValue temp;
+/*	CTempValue temp;
 	temp.val1 = ts.val1 * 2;
 	temp.val2 = ts.val2 * 2;
 
-	return temp; //生成临时对象，将temp中的数据拷贝到临时对象，返回临时对象
+	return temp;*/ //生成临时对象，将temp中的数据拷贝到临时对象，返回临时对象
+
+	//优化
+	return CTempValue(ts.val1 * 2, ts.val2 * 2);
+}
+
+//编译器视角
+void Double(CTempValue& tempObj, CTempValue& ts)
+{
+	tempObj.CTempValue::CTempValue(ts.val1 * 2, ts.val2 * 2);
 }
 
 int main()
@@ -39,9 +49,17 @@ int main()
 	//编译器角度优化和开发者角度优化
 
 	//1.开发者层面的优化
-	CTempValue ts1(10, 20);
-	Double(ts1); //返回值没人接会析构临时对象
-	CTempValue tsw = Double(ts1); //如果返回的临时对象有人接，则临时对象的析构不会被调用
+	//CTempValue ts1(10, 20);
+	//Double(ts1); //返回值没人接会析构临时对象
+	//CTempValue tsw = Double(ts1); //如果返回的临时对象有人接，则临时对象的析构不会被调用
+
+	//编译器视角
+	//CTempValue ts1;
+	//ts1.CTempValue::CTempValue(10, 20);
+	//CTempValue tempObj;
+	//Double(tempObj, ts1);
+
+	//2.编译器层面，优化
 
 	return 0;
 }
